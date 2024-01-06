a27a0809bc6c9e6536278e0e49202def053b3ee3
MohammedGhazaly <warendragon67@gmail.com>
2024-01-06 22:12:11 +0200

    Created models

--- lib/features/weather_search/presentation/views/widgets/weather_search_body.dart
+++ lib/features/weather_search/presentation/views/widgets/weather_search_body.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:weather_app/features/weather_search/presentation/views/widgets/city_location_widget.dart';
-import 'package:weather_app/features/weather_search/presentation/views/widgets/custom_loading_indicator.dart';
 import 'package:weather_app/features/weather_search/presentation/views/widgets/custom_search_field.dart';
 import 'package:weather_app/features/weather_search/presentation/views/widgets/success_widgets.dart';
-import 'package:weather_app/features/weather_search/presentation/views/widgets/weather_card.dart';
-import 'package:weather_app/features/weather_search/presentation/views/widgets/weather_condition_section.dart';
-import 'package:weather_app/utils/app_colors.dart';
-import 'package:weather_app/utils/app_strings.dart';
-import 'package:weather_app/utils/common_widgets/error_widget.dart';
 
 class WeatherSearchBody extends StatelessWidget {
   const WeatherSearchBody({super.key});
--- lib/services/remote/api_services.dart
+++ lib/services/remote/api_services.dart
@@ -3,8 +3,12 @@ import 'package:dio/dio.dart';
 class ApiServices {
   final Dio dio = Dio();
 
-  Future<Response<dynamic>> getData({required String path}) async {
-    final response = await dio.get(path);
+  Future<Response<dynamic>> getData(
+      {required String path, Map<String, dynamic>? queryParameters}) async {
+    final response = await dio.get(
+      path,
+      queryParameters: queryParameters,
+    );
     return response;
   }
 }
